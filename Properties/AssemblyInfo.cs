71af81e16d63ed6d95cebd56c8ecd58f8b608c99
Jori Huisman <jori@huisman.io>
2026-03-10 14:55:25 +0100

    Sync assembly version with release version from CI pipeline

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -21,16 +21,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("78af2871-0ceb-4451-b80d-455552e37c91")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
